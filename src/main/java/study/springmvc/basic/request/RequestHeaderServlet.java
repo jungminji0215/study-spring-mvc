e483ef58c166009e6ad255bf919b9c06653782e1
jungminji <wjdalswl0215@gmail.com>
2022-01-13 01:13:44 +0900

    HTTP 요청 데이터 - GET 쿼리 파라미터

--- src/main/java/study/springmvc/basic/request/RequestHeaderServlet.java
+++ src/main/java/study/springmvc/basic/request/RequestHeaderServlet.java
@@ -1,8 +1,10 @@
 package study.springmvc.basic.request;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //http://localhost:8080/request-header?username=hello
@@ -10,7 +12,7 @@ import java.io.IOException;
 public class RequestHeaderServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         // http 요청 메세지의 첫 라인
         printStartLine(request);
@@ -29,7 +31,7 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printStartLine(HttpServletRequest request){
         System.out.println("--- REQUEST-LINE - start ---");
         System.out.println("request.getMethod() = " + request.getMethod()); // GET
-        System.out.println("request.getProtocal() = " + request.getProtocol()); // HTTP/1.1
+        System.out.println("request.getProtocol() = " + request.getProtocol()); // HTTP/1.1
         System.out.println("request.getScheme() = " + request.getScheme()); // http
 
         // http://localhost:8080/request-header
@@ -40,7 +42,7 @@ public class RequestHeaderServlet extends HttpServlet {
 
         //username=hi
         System.out.println("request.getQueryString() = " + request.getQueryString());
-        System.out.println("request.isSecure() = " + request.isSecure()); //https사용 유무
+        System.out.println("request.isSecure() = " + request.isSecure()); // https 유무
         System.out.println("--- REQUEST-LINE - end ---");
         System.out.println();
     }
